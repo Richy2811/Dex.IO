751f27b61117d8231ac0d3ddc65612615e12ffbc
Richy <richardloeffler14@gmail.com>
2025-01-13 22:46:43 +0100

    Changed sprite orientation and window size

--- src/main/java/at/fhtw/dexio/DexIOApplication.java
+++ src/main/java/at/fhtw/dexio/DexIOApplication.java
@@ -11,11 +11,11 @@ public class DexIOApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DexIOApplication.class.getResource("DexIOView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 620);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("DexIO");
         stage.setScene(scene);
-        stage.setMinWidth(700);
-        stage.setMinHeight(500);
+        stage.setMinWidth(750);
+        stage.setMinHeight(650);
         stage.show();
     }
 
